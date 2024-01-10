6367d93eefe36560bdcc84939c0f0461897988c0
TOM-TITO-coder <titotom934@gmail.com>
2024-01-10 08:28:04 +0700

    auth/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,15 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/categories', [CategoryController::class,'getCategories']);
-Route::get('/categories', [CategoryController::class,'createCategories']);
-Route::get('/categories/{categoryId}', 'App\Http\Controllers\CategoryController@getCategory');
-Route::get('/categories/{categoryId}/edit', [CategoryController::class,'updateCategory']);
-Route::get('/categories/{categoryId}/delete', [CategoryController::class,'deleteCategory']);
-Route::get('/categories/{categoryId}/products', [ProductController::class,'getCategoryProducts']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/products', [ProductController::class,'getProducts']);
-Route::get('/products', [ProductController::class,'createProduct']);
-Route::get('/products/{product}', [ProductController::class,'getProduct']);
-Route::get('/products/{product}/edit', [ProductController::class,'updateProduct']);
-Route::get('/products/{product}/delete', [ProductController::class,'deleteProduct']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
